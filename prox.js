b88c48318c8f6c8f8c4e7957263fea9b35f48ad5
Bjartwolf <bjartnes@gmail.com>
2012-08-21 13:02:36 +0200

    proxy working

--- prox.js
+++ prox.js
@@ -5,18 +5,22 @@ var http = require('http')
   , responseBodyLogger = new (winston.Logger)({ transports: [ new (winston.transports.File)({ filename: 'logs/http-response-body.log' }) ]});
 
 http.createServer(function (req, res) {
-	var serverUrl = 'http://localhost:1338';
-	var url = serverUrl + req.url;
-	var requester = request[req.method.toLowerCase()];
-	var x = requester(url);
-	req.on('data', function (chunk) {
-		logger.log("info", "REQUEST BODY " + chunk);
+    var serverUrl = 'http://localhost:1338';
+    var url = serverUrl + req.url;
+    // Just an hack to workaround jquery sending posts as options
+    if (req.method === 'OPTIONS') {
+        req.method = 'POST';
+    }
+    var requester = request[req.method.toLowerCase()];
+    var x = requester(url);
+    req.on('data', function (chunk) {
+        logger.log("info", "REQUEST BODY " + chunk);
     });
-	x.on('data', function (chunk) {
-		responseBodyLogger.log("info", "RESPONSE BODY " + chunk);
+    x.on('data', function (chunk) {
+        responseBodyLogger.log("info", "RESPONSE BODY " + chunk);
     });
-	logger.log("info", req.url + " | " + req.method);
-	req.pipe(x);
-	x.pipe(res);
+    logger.log("info", req.url + " | " + req.method);
+    req.pipe(x);
+    x.pipe(res);
 }).listen(8337, '127.0.0.1');
 
